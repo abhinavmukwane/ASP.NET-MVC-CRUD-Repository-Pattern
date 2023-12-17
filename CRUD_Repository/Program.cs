5eda6561348cd3cc82e9a8045c0096147f87039b
abhinavmukwane <112157328+abhinavmukwane@users.noreply.github.com>
2023-12-17 12:22:49 +0530

    Update Project

--- CRUD_Repository/Program.cs
+++ CRUD_Repository/Program.cs
@@ -10,7 +10,7 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddControllersWithViews();
 
 //builder.Services.AddDbContext<AppDbContext>(options => options.UseSqlServer(builder.Configuration.GetConnectionString("Database")));
-builder.Services.AddDbContext<AppDbContext>(options => options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection2")));
+builder.Services.AddDbContext<AppDbContext>(options => options.UseSqlServer(builder.Configuration.GetConnectionString("Database")));
 
 builder.Services.AddScoped<IProductRepository, ProductRepository>();
 
